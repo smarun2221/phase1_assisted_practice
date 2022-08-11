1fdec88c24869eb0a0728d448a3afce56cf296e0
smarun2221 <110038282+smarun2221@users.noreply.github.com>
2022-08-11 04:04:12 -0700

    Add files via upload

---
+++ Queue1.java
@@ -0,0 +1,21 @@
+package phase1_assisted_practice;
+
+import java.util.Queue;
+import java.util.LinkedList;
+public class Queue1 
+{
+public static void main(String[] args) 
+{
+        		Queue<String> locationsQueue = new LinkedList<String>();
+        		locationsQueue.add("Kolkata");
+        		locationsQueue.add("Patna");
+        		locationsQueue.add("Delhi");
+        		locationsQueue.add("Gurgaon");
+        		locationsQueue.add("Noida");
+        		System.out.println("Queue is : " + locationsQueue);
+        		System.out.println("Head of Queue : " + locationsQueue.peek());
+        		locationsQueue.remove();
+        		System.out.println("After removing Head of Queue : " + locationsQueue);
+        		System.out.println("Size of Queue : " + locationsQueue.size());
+    	}
+}
